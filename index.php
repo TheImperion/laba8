9ddab0ae794cf52ceaacf67ef235a7b71fab6663
TheImperion <majorXL1@yandex.ru>
2019-12-06 22:09:42 +0300

    zaliv

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Главная страница</title>
+</head>
+<body>
+
+	<a href="/zadanie1/basket.html">Задание №1</a>
+	<a href="/zadanie2/form.html">Задание №2</a>
+
+</body>
+</html>
